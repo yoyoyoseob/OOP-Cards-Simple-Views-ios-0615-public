9382070edbba621ae2303d1ed6631c4f990de729
yoyoyoseob <yoyoyoseob@gmail.com>
2015-06-11 11:57:30 -0400

    cards working as intended

--- OOPCardsSimpleViews/CardSwitcherViewController.h
+++ OOPCardsSimpleViews/CardSwitcherViewController.h
@@ -9,5 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface CardSwitcherViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *topLabel;
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomLabel;
+@property (weak, nonatomic) IBOutlet UIButton *spadesButton;
+@property (weak, nonatomic) IBOutlet UIButton *clubsButton;
+@property (weak, nonatomic) IBOutlet UIButton *diamondsButton;
+@property (weak, nonatomic) IBOutlet UIButton *heartsButton;
+
+- (IBAction)threeOfSpadesButton:(id)sender;
+- (IBAction)fourOfClubsButton:(id)sender;
+- (IBAction)eightOfDiamondsButton:(id)sender;
+- (IBAction)tenOfHeartsButton:(id)sender;
 
 @end
--- OOPCardsSimpleViews/CardSwitcherViewController.m
+++ OOPCardsSimpleViews/CardSwitcherViewController.m
@@ -17,8 +17,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self.view bringSubviewToFront:self.spadesButton];
+    [self.view bringSubviewToFront:self.clubsButton];
+    [self.view bringSubviewToFront:self.diamondsButton];
+    [self.view bringSubviewToFront:self.heartsButton];
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)threeOfSpadesButton:(id)sender {
+    self.topLabel.text = self.spadesButton.titleLabel.text;
+    self.middleLabel.text = self.spadesButton.titleLabel.text;
+    self.bottomLabel.text = self.spadesButton.titleLabel.text;
+}
+
+- (IBAction)fourOfClubsButton:(id)sender {
+    self.topLabel.text = self.clubsButton.titleLabel.text;
+    self.middleLabel.text = self.clubsButton.titleLabel.text;
+    self.bottomLabel.text = self.clubsButton.titleLabel.text;
+}
+
+- (IBAction)eightOfDiamondsButton:(id)sender {
+    self.topLabel.text = self.diamondsButton.titleLabel.text;
+    self.middleLabel.text = self.diamondsButton.titleLabel.text;
+    self.bottomLabel.text = self.diamondsButton.titleLabel.text;
+}
+
+- (IBAction)tenOfHeartsButton:(id)sender {
+    self.topLabel.text = self.heartsButton.titleLabel.text;
+    self.middleLabel.text = self.heartsButton.titleLabel.text;
+    self.bottomLabel.text = self.heartsButton.titleLabel.text;
+}
 
 @end
